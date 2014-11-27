e1b3f1582383eabf54e33920683de1cc3418df03
Oliver Delevingne <oliver@delevingne.co.uk>
2014-11-27 22:06:59 +0000

    display search term

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,20 @@
 $(document).ready(function(){
+
   var $searchArtistForm = $("#search-artist-form");
+  var $searchArtistName = $("#search-artist-name");
+  var $searchTerm = $("#search-term");
+  var artistName;
+
   $searchArtistForm.on("submit", function(event){
     event.preventDefault();
-    console.info("submitted form");
-  })
+
+    artistName = $searchArtistName.val();
+    $searchTerm.html(artistName);
+    
+    console.info("Searched for", artistName);
+  });
+   
+
+
 });
+
