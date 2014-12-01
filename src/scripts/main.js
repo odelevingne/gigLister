81525383ca58920e46e3ca8ecaea96fbf9906144
Oliver Delevingne <oliver@delevingne.co.uk>
2014-12-01 18:50:07 +0000

    can select correct artist match. makes successful request to artist calendar api

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -30,16 +30,27 @@ $(document).ready(function(){
   var createArtistList = function(artists) {
     var bestMatches = artists.slice(0,5)
 
-    var template = "<li> {{displayName}} </li>";
+    var artistTemplate = "<li> <a href='{{id}}' id='confirm-artist'> {{displayName}} </a> </li>";
 
     $.each(bestMatches, function(i, item){
+      var artistId = bestMatches[i].id
+      var renderedArtists = Mustache.render(artistTemplate, item);
 
-      var renderedArtists = Mustache.render(template, item)
       $('#artist-results').append(renderedArtists);
+
+      $('#confirm-artist').on("click", function(event) {
+        event.preventDefault();
+        getArtistCalender(href);
+
+      }); 
+
+      var href = $("#confirm-artist").attr('href');
+
 
     });
     console.log(artists);
   };
+
 
   $searchArtistForm.on("submit", function(event){
     event.preventDefault();
@@ -52,7 +63,22 @@ $(document).ready(function(){
     searchSongKick(artistName);
     
   });
-  
+
+  getArtistCalender = function(href){
+    $.ajax({
+      type: 'GET',
+      url: "http://api.songkick.com/api/3.0/artists/"+href+"/calendar.json?apikey=",
+      data: {
+        apikey: SONG_KICK_API_KEY
+      },
+      success: function(resp) {
+        console.log(resp.resultsPage.results.event);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.error("Nooo!!", jqXHR, textStatus, errorThrown);
+      }
+    });
+  };
 
 });
 
